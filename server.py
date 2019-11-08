52feb2d5e9a4d37c0145a14e200d9260ba159c69
Fatih <fatihcm7@gmail.com>
2019-11-08 15:15:00 +0300

    credit card rule added

--- server.py
+++ server.py
@@ -6,8 +6,9 @@ def create_app():
     app = Flask(__name__)
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/blog", view_func=views.blog_page)
-    app.add_url_rule("/profile", view_func=views.profile_page)
-    app.add_url_rule("/signup", view_func=views.signupbase_page, methods=["GET","POST"])
+    app.add_url_rule("/profile", view_func=views.profile_page, methods=["GET", "POST"])
+    app.add_url_rule("/addcreditcard", view_func=views.addcreditcard_page, methods=["GET", "POST"])
+    app.add_url_rule("/signup", view_func=views.signupbase_page, methods=["GET", "POST"])
 
     app.config["DEBUG"] = True
     return app

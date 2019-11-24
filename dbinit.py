cf91522b4a20ba4bf820e9a0fde7ee392fd7f16f
semihucann <ucans.itu@gmail.com>
2019-11-24 15:17:26 +0300

    creating people table and test

--- dbinit.py
+++ dbinit.py
@@ -33,8 +33,25 @@ INIT_STATEMENTS = [
         twitter VARCHAR (500),
         instagram VARCHAR (500)
     )""",
-
-
+    #SEMİH TABLE ######################################################################
+    #"CREATE TYPE IF NOT EXISTS GENDER_TYPE AS ENUM ('male', 'female', 'unisex'); ",
+    #"CREATE TYPE IF NOT EXISTS ROLE AS ENUM ('berber','owner','user', 'admin' ); ",
+    #One char is used
+    """
+    CREATE TABLE IF NOT EXISTS People(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE,
+        name_surname VARCHAR(50),
+        mail VARCHAR(300) UNIQUE,
+        password_hash VARCHAR(300),
+        gender VARCHAR(1),
+        age integer,
+        role VARCHAR(1)    
+    )""",
+    """
+    INSERT INTO People(username,name_surname,mail,password_hash,gender,age,role) VALUES('firstuser','first user','first@user.com', '1a2b','m',22,'u');
+    """
+    #################################################################################
 ]
 
 
@@ -47,7 +64,7 @@ def initialize(url):
 
 
 if __name__ == "__main__":
-    url = "postgres://lltjryrurbhacv:4ed89e50a1718d204c9ac3cee26560e3874bf47bd7ce47e12f0c4f81611954f6@ec2-107-22-236-52.compute-1.amazonaws.com:5432/d2lpnjbrjgerqm"
+    url=os.getenv("DATABASE_URL")
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)

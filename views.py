e643719e3c446cab52a2f0bfd2ed1a26f984f4db
semihucann <ucans.itu@gmail.com>
2019-11-26 17:08:08 +0300

    creating peoplemodel for queries

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from flask import render_template,Flask,request,redirect,url_for
 import Temporarypython
-
+from Models import Peoplemodel
+from Entities import Comment, ContactInfo, Rezervation, People, Berber, Owner
 
 def home_page():
     berbershopList = Temporarypython.listOfBerbers()
@@ -81,12 +82,21 @@ def signup_user_page():
     if request.method == 'GET':
         return render_template("signup_user.html")
     else:
-        form_mail = request.form["mail"]
-        form_name_surname = request.form["name_surname"]
-        form_username = request.form["username"]
-        form_password = request.form["password"]
-        form_gender = request.form["gender"]
-        print(form_mail, form_name_surname, form_username, form_password, form_gender)
+        person = People()
+        person.username = request.form["username"]
+        person.name_surname = request.form["name_surname"]
+        person.mail = request.form["mail"]
+        person.password_hash = request.form["password"]
+        person.gender = request.form["gender"]
+        person.age = request.form["age"]
+        person.role = "user"
+        #print(person.username, person.name_surname, person.mail, person.password_hash, person.gender, person.age, person.role)
+        people = Peoplemodel()
+        people.insert(person)
+
+
+
+
         return redirect(url_for("home_page"))
         # uyarı metni yazmamız gerekiyor
 
